298beae359e62cee13f3a71eec3bcae342cc2536
jatrinahidhasanlimon <97885678+jatrinahidhasanlimon@users.noreply.github.com>
2022-08-30 18:13:24 +0600

    nested route testing

---
+++ components/CategoryCard.vue.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="shop-widget">
+      <h3 class="shop-title">Club</h3>
+      <ul class="shop-link" v-for="club in clubs" :key="club._id">
+          <li><input
+              id="toy"
+              type="checkbox"
+              v-model="checkedClubs"
+              :value="club._id"
+              class="w-5 h-5 border-gray-300 rounded"
+            /> {{club.name}}</li>
+          
+      </ul>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default{
+name: 'ClubCard',
+props: [''],
+components:{
+    
+},
+data() {
+  return {
+    checkedClubs : [],
+  }
+},
+computed: {
+  clubs() {
+      return this.$store.state.club.clubs;
+    },
+    
+},
+mounted() {
+    this.$store.dispatch("club/fetchclubs")  
+},
+  methods: {
+  },
+watch: {
+    checkedClubs:{
+      handler: function(){
+         this.$store.dispatch("product/fetchProducts",this.$updateQueryParams('club',this.checkedClubs))
+      }
+    }
+  },
+created() {
+  this.checkedClubs = this.$revertSpecificQueryParamsToArray('club')
+},
+
+}
+</script>
---
+++ pages/_category/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ product }} / {{ slug }}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+  return {
+    showProductQuickViewModal: false,
+    products_check: [],
+    clicked_product: null,
+  }
+},
+    async asyncData({ params }) {
+        const product = params.category
+        const slug = params.slug
+        return { product, slug }
+}
+    }
+  </script>
